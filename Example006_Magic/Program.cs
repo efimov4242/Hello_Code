2a5631f73d9bdf1d8be8f0de5bb885866f9efc32
Ilya <efimov4242@gmail.com>
2022-10-13 09:11:21 +0400

    Поиск макимального числа

---
+++ Example006_Magic/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 2;
+int b = 4;
+int c = 20;
+int d = 6;
+int e = 16;
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (e>max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
