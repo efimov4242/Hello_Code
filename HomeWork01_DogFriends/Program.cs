8ec5dc133a1411ba9b4d98b4d1604306c5edd2f8
Ilya <efimov4242@gmail.com>
2022-10-13 15:40:36 +0400

    HomeWork01_DogFriend

---
+++ HomeWork01_DogFriends/Program.cs
@@ -0,0 +1,24 @@
+﻿int friend = 2;
+int first_friend_speed = 1;
+int second_friend_speed = 2;
+int dog_speed = 20;
+int distance = 10000;
+int count = 0;
+int time = 0;
+
+while (distance > 2)
+{
+    if (friend == 2)
+    {
+        time = distance / (dog_speed + second_friend_speed);
+        friend = 1;
+    }
+    else
+    {
+        time = distance / (dog_speed + first_friend_speed);
+        friend = 2;
+    }
+    distance = distance - (first_friend_speed - second_friend_speed) * time;
+    count++;
+}
+Console.WriteLine(count);
